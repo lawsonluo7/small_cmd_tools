ac8667d1e0834567a8118752061350f6da9d16b9
Lawson Luo <lawsonluo7@gmail.com>
2025-02-23 12:57:56 -0800

    Added Function usage
    Added function usage without ""
    Elaborated documentation on operators
    Changed exponent operator from ** to ^
    Moved things around for organiztion and clarity

--- calc.py
+++ calc.py
@@ -1,9 +1,19 @@
 import argparse
-parser = argparse.ArgumentParser(description="Evaluate a mathematical equation.")
+parser = argparse.ArgumentParser(description="""
+Evaluate a mathematical equation.
+Operators: +, -, *, /, ^, %, //, =
+Functions, use [] for parameters:
+abs, round, pow, max, min, sum, sqrt, log, log10, log2, exp, sin, cos, tan,
+asin, acos, atan, sinh, cosh, tanh, asinh, acosh, atanh, tau, inf, nan""")
 parser.add_argument('equation', type=str, help="The equation to evaluate")
 args = parser.parse_args()
-eq = args.equation
+prnt_eq = args.equation
+eval_eq = (prnt_eq
+    .replace('=', '==')
+    .replace('^', '**')
+    .replace('[', '(').replace(']',')')
+    )
 try:
-    print(f"{eq} is {eval(eq.replace('=', '=='))}")
+    print(f"{prnt_eq} is {eval(eval_eq)}")
 except Exception as error:
     print(f"Error evaluating equation: {error}")
