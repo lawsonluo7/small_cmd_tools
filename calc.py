c2349eb7b548cc6a1ac8cf930270d3a5f5f4d051
Lawson Luo <lawsonluo7@gmail.com>
2025-02-23 13:03:52 -0800

    Added Isolated Variable handling

--- calc.py
+++ calc.py
@@ -6,6 +6,9 @@ Functions, use [] for parameters:
 abs, round, pow, max, min, sum, sqrt, log, log10, log2, exp, sin, cos, tan,
 asin, acos, atan, sinh, cosh, tanh, asinh, acosh, atanh, tau, inf, nan""")
 parser.add_argument('equation', type=str, help="The equation to evaluate")
+parser.add_argument('-v', '--variable', action='store_true',
+    help="whether there is a variables, variable must be already isolated"
+    )
 args = parser.parse_args()
 prnt_eq = args.equation
 eval_eq = (prnt_eq
@@ -13,6 +16,8 @@ eval_eq = (prnt_eq
     .replace('^', '**')
     .replace('[', '(').replace(']',')')
     )
+if args.variable:
+    eval_eq = eval_eq.strip('x=')
 try:
     print(f"{prnt_eq} is {eval(eval_eq)}")
 except Exception as error:
